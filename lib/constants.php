a4f8ccf670aeab1415006c792bda97f20142cfb2
dtelepathy <dtelepathy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-07 23:31:24 +0000

    Initial Commit
    git-svn-id: https://plugins.svn.wordpress.org/author-recommended-posts/trunk@635617 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/constants.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Constants used by this plugin
+ * 
+ * @package AuthorRecommendedPosts
+ * 
+ * @author digital-telepathy
+ * @version 1.0.0
+ * @since 1.0.0
+ */
+
+// The current version of this plugin
+if( !defined( 'AUTHOR_RECOMMENDED_POSTS_VERSION' ) ) define( 'AUTHOR_RECOMMENDED_POSTS_VERSION', '1.0.0' );
+
+// The directory the plugin resides in
+if( !defined( 'AUTHOR_RECOMMENDED_POSTS_DIRNAME' ) ) define( 'AUTHOR_RECOMMENDED_POSTS_DIRNAME', dirname( dirname( __FILE__ ) ) );
+
+// The URL path of this plugin
+if( !defined( 'AUTHOR_RECOMMENDED_POSTS_URLPATH' ) ) define( 'AUTHOR_RECOMMENDED_POSTS_URLPATH', WP_PLUGIN_URL . "/" . plugin_basename( AUTHOR_RECOMMENDED_POSTS_DIRNAME ) );
+
+if( !defined( 'IS_AJAX_REQUEST' ) ) define( 'IS_AJAX_REQUEST', ( !empty( $_SERVER['HTTP_X_REQUESTED_WITH'] ) && strtolower( $_SERVER['HTTP_X_REQUESTED_WITH'] ) == 'xmlhttprequest' ) );
